de81b9e92928cabd46812fca74832f712d4fc2da
Pablo Monfort <pmonfort@gmail.com>
2026-04-14 11:15:58 -0300

    Fix rake task load path in railtie

--- lib/schemerd/railtie.rb
+++ lib/schemerd/railtie.rb
@@ -3,7 +3,7 @@
 module Schemerd
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.expand_path("../../tasks/schemerd.rake", __dir__)
+      load File.expand_path("../tasks/schemerd.rake", __dir__)
     end
   end
 end
